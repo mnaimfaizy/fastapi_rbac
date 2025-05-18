1e168b800eb4a0f9ff157b471de0c083a8228d5c
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-05-18 18:31:07 +1000

    Fix: Use correct docker-compose file (prod-test.yml) in push.sh

--- push-to-dockerhub.sh
+++ push-to-dockerhub.sh
@@ -31,11 +31,11 @@ echo -e "\\033[33mUsing tag: ${TAG}\\033[0m"
 
 # Stop any running containers
 echo -e "\\033[32mStopping any running containers...\\033[0m"
-docker compose -f docker-compose.prod.yml down --remove-orphans
+docker compose -f docker-compose.prod-test.yml down --remove-orphans
 
 # Build the production images
 echo -e "\\033[32mBuilding production Docker images...\\033[0m"
-docker compose -f docker-compose.prod.yml build
+docker compose -f docker-compose.prod-test.yml build
 
 # Tag images for DockerHub
 echo -e "\\033[32mTagging images for DockerHub with tag ${TAG}...\\033[0m"
