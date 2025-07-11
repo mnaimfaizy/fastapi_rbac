50bc6861df92c8e4e036cf2a09478d207a81856a
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-07-11 11:06:12 +1000

    🐍 chore(backend): update __init__.py import statements for CRUD modules

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,5 +1,5 @@
-from .permission_crud import permission
-from .permission_group_crud import permission_group
-from .role_crud import role
-from .role_group_crud import role_group
-from .user_crud import user
+from .permission_crud import permission  # noqa: F401
+from .permission_group_crud import permission_group  # noqa: F401
+from .role_crud import role  # noqa: F401
+from .role_group_crud import role_group  # noqa: F401
+from .user_crud import user  # noqa: F401
